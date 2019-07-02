0cc96334ee32138c7190be84d5cb83cc746ebe87
dayton brock <dayton.brock@gmail.com>
2019-07-02 14:36:56 -0500

    add HomePage component, build out HomePage wireframe with mock data

--- src/components/App/App.js
+++ src/components/App/App.js
@@ -16,6 +16,7 @@ import ProtectedRoute from '../ProtectedRoute/ProtectedRoute'
 import AboutPage from '../AboutPage/AboutPage';
 import UserPage from '../UserPage/UserPage';
 import InfoPage from '../InfoPage/InfoPage';
+import HomePage from '../HomePage/HomePage';
 
 import './App.css';
 
@@ -43,10 +44,16 @@ class App extends Component {
             Visiting localhost:3000/home will show the UserPage if the user is logged in.
             If the user is not logged in, the ProtectedRoute will show the 'Login' or 'Register' page.
             Even though it seems like they are different pages, the user is always on localhost:3000/home */}
-            <ProtectedRoute
+            {/* <ProtectedRoute
               exact
               path="/home"
               component={UserPage}
+            /> */}
+            {/* The new "HomePage" that will display all the reviews from the database */}
+            <ProtectedRoute
+              exact
+              path="/home"
+              component={HomePage}
             />
             {/* This works the same as the other protected route, except that if the user is logged in,
             they will see the info page instead. */}
---
+++ src/components/HomePage/HomePage.js
@@ -0,0 +1,98 @@
+// src/components/HomePage/HomePage.js
+import React, { Component } from 'react';
+import { connect } from 'react-redux';
+
+// Material-UI components
+import Grid from '@material-ui/core/Grid';
+import Card from '@material-ui/core/Card';
+import CardContent from '@material-ui/core/CardContent';
+import Button from '@material-ui/core/Button';
+import TextField from '@material-ui/core/TextField'
+
+class HomePage extends Component {
+    render() {
+        return (
+            <div className="App">
+                <h3>Recent Reviews</h3>
+                <Grid container
+                    spacing={5}>
+                    <Grid item xs={4}>
+                        <Card>
+                            <CardContent>
+                                <header>
+                                    <h4>Theodore Wirth Park</h4>
+                                </header>
+                                <p>snippet of review text...</p>
+                            </CardContent>
+                        </Card>
+                    </Grid>
+                    <Grid item xs={4}>
+                        <Card>
+                            <CardContent>
+                                <header>
+                                    <h4>Bryn Mawr Meadows</h4>
+                                </header>
+                                <p>snippet of review text...</p>
+                            </CardContent>
+                        </Card>
+                    </Grid>
+                    <Grid item xs={4}>
+                        <Card>
+                            <CardContent>
+                                <header>
+                                    <h4>Clinton Field Park</h4>
+                                </header>
+                                <p>snippet of review text...</p>
+                            </CardContent>
+                        </Card>
+                    </Grid>
+                    <Grid item xs={4}>
+                        <Card>
+                            <CardContent>
+                                <header>
+                                    <h4>Elliot Park</h4>
+                                </header>
+                                <p>snippet of review text...</p>
+                            </CardContent>
+                        </Card>
+                    </Grid>
+                    <Grid item xs={4}>
+                        <Card>
+                            <CardContent>
+                                <header>
+                                    <h4>Mueller Park</h4>
+                                </header>
+                                <p>snippet of review text...</p>
+                            </CardContent>
+                        </Card>
+                    </Grid>
+                    <Grid item xs={4}>
+                        <Card>
+                            <CardContent>
+                                <header>
+                                    <h4>Powderhorn Park</h4>
+                                </header>
+                                <p>snippet of review text...</p>
+                            </CardContent>
+                        </Card>
+                    </Grid>
+                    {/* map through actual reviews */}
+                </Grid>
+                <br/>
+                <br/>
+                <Grid container>
+                    <Grid item xs={6}>
+                        <Button>
+                            Add A New Review
+                        </Button>
+                    </Grid>
+                    <Grid item xs={6}>
+                        <TextField placeholder="Search by park name"/>
+                    </Grid>
+                </Grid>
+            </div>
+        );
+    }
+}
+
+export default connect()(HomePage);
