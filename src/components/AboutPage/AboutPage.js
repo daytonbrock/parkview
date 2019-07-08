42ea9445dca13597fdbbaccaced448ee01988214
dayton brock <dayton.brock@gmail.com>
2019-07-08 13:21:55 -0500

    add draft for about page

--- src/components/AboutPage/AboutPage.js
+++ src/components/AboutPage/AboutPage.js
@@ -7,9 +7,20 @@ import React from 'react';
 
 const AboutPage = () => (
   <div>
-    <div>
+    <div className="App">
+      <h3>Overview</h3>
       <p>
-        This about page is for anyone to read!
+        ParkView aims to provide community members with a platform to communicate
+        for themselves their values and priorities
+        for the parks in their neighborhoods that extends beyond community engagement practices common today. At its core, ParkView is a park review application– allowing users to search
+        for a park and leave a review, as well as read other community members reviews.
+      </p>
+      <h3>Background</h3>
+      <p>
+        After years of underfunding and unequal access to parks and public green spaces, we find ourselves in a moment where significant local and national efforts are being made to reverse this and rehabilitate parks across the United States with careful research that focuses on equity defined as fair and just access to opportunities and resources. ParkView provides community members with a platform to participate in the conversation by communicating
+        for themselves their values and priorities
+        for the parks in their neighborhoods that extends beyond community engagement practices common today. At its core, ParkView is a park review application– allowing users to search
+        for a park and leave a review, as well as read other community members reviews.
       </p>
     </div>
   </div>
