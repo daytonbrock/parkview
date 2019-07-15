5f24b0a5d250327843d8abb8fe45757266042e56
dayton brock <dayton.brock@gmail.com>
2019-07-15 00:26:14 -0500

    add Material-UI Card to ReviewImages

--- src/components/ReviewImages/ReviewImages.js
+++ src/components/ReviewImages/ReviewImages.js
@@ -4,7 +4,9 @@ import { connect } from 'react-redux';
 
 // Material-UI components
 import Grid from '@material-ui/core/Grid';
-import Button from '@material-ui/core/Button';
+import Card from '@material-ui/core/Card';
+import CardContent from '@material-ui/core/CardContent';
+import CardMedia from '@material-ui/core/CardMedia';
 
 class ReviewImages extends Component {
 
@@ -19,11 +21,16 @@ class ReviewImages extends Component {
     render() {
         return (
             <div className="App">
-                <Grid container>
+                <Grid container spacing={5}>
                     {this.props.state.reviewImages.map( image => {
                         return (
                             <Grid item xs={12}>
-                                <img alt={image.name} src={image.url}/>
+                                {/* <img className="review-image" alt={image.name} src={image.url}/> */}
+                                <Card>
+                                    <CardMedia component="img"
+                                        alt={image.name} 
+                                        src={image.url}/>
+                                </Card>
                             </Grid>
                         );
                     })}
